5efc53d3b3364459ca1380bb66d7cff1598e2bcd
EastonHMorrill <emorrill@gus.pittstate.edu>
2021-03-19 12:35:00 -0500

    Nearly completed code! The current error tells me that line 22 is attempting to access an index of an array that is out of the range. 'IndexError: list index out of range.' Otherwise I'm extremely close. Need to take a look at the output of the pressure array.

--- MorrillPhantomBlackHole.py
+++ MorrillPhantomBlackHole.py
@@ -1,31 +1,49 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 G = 6.6743E-11 # in N*m^2/Kg^2
 c = 2.998E8 # in m/s
 M_NS = 1.4 # in Solar Masses
 R_NS = 10000 # Approximate radius in m
-NDP = 1.44 # Pressure given in Solar Masses
 
-r = np.linspace(0, 10000, num=1000) # in m
-def P_Tol(r):
-    return ((15*M_NS)/(8*math.pi*(R_NS)**3))*(1-(r/R_NS)**2)
+NDP = []
+for i in range(0,1001):
+    NDP.append(1.44) # Pressure given in Solar Masses
 
-#outFile = open("PhantomBlackHoleRadiusData.txt", "w")
-#for i in range(0, 1000):
-#    if (P_Tol[i] > NDP):
-#        outFile.write("The required pressure exists within the Neutron Star at a radius of: " + 10*i + "\n")
-#    else:
-#        outFile.write("The required pressure does not exist within the Neutron Star." + "\n")
-#outFile.close()
+rho_Tol = []
+P = []
+r = np.linspace(0, 10000, num=1001) # in m
+for i in range(0, 1001):
+    if (r[i]==0):
+        P.append(0.0)
+    else:
+        rho_Tol.append(((15*M_NS)/(8*math.pi*(R_NS)**3))*(1-((r[i])/R_NS)**2)) # Output in Solar masses/m^3
+        P.append((rho_Tol[i])*(G*M_NS/((r[i])**2))*(R_NS-(r[i]))) # Output in 
 
-M = np.linspace(0, 3, num=1000) # in Solar Masses
-def r_Sch(M):
-    return (2*(M*(1.989E30))*G)/(c**2) # Includes conversion, outputs in m
-def M_Tol(r_Sch):
-    return M_NS*((5/2)*(r_Sch/R_NS)**3 - (3/2)*(r_Sch/R_NS)**5) # Outputs in Solar Masses
+outFile = open("PhantomBlackHolePressureData.txt", "w")
+for i in range(0, 1001):
+    if (P[i] > NDP[i]):
+        outFile.write("The required pressure exists within the Neutron Star at a radius of: " + str(10*i) + "m" + "\n")
+    else:
+        outFile.write("The requred pressure does not exist at a radius of: " + str(10*i) + "m" + "\n")
+outFile.close()
+
+plt.plot(r, P)
+plt.plot(r, NDP)
+plt.savefig("PressurePlot.pdf")
+plt.close()
+
+M = np.linspace(0, 3, num=1001) # in Solar Masses
+r_Sch = (2*(M*(1.989E30))*G)/(c**2) # Includes conversion, outputs in m
+M_Tol = M_NS*((5/2)*(r_Sch/R_NS)**3 - (3/2)*(r_Sch/R_NS)**5) # Outputs in Solar Masses
 
 outFilenew = open("PhantomBlackHoleMassData.txt", "w")
-for i in range(0, 1000):
-    outFilenew.write(str(M[i]) + " " + str(M_Tol[i]) + "\n")
+for i in range(0, 1001):
+    outFilenew.write(str(M[i]) + " " + str(M_Tol[i]) + " " + str(r[i]) + "\n")
 outFilenew.close() 
+
+plt.plot(r_Sch, M)
+plt.plot(r_Sch, M_Tol)
+plt.savefig("MassPlot.pdf")
+plt.close()
