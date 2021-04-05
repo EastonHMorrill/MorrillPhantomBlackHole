0713f053e1623be657f68c30b15a3b23d7f3450d
EastonHMorrill <emorrill@gus.pittstate.edu>
2021-04-05 15:02:02 -0500

    Working NSMass file with output plot given in SI units. Perfection. Not working NSPressure file. I replaced all the instances of rho_c*R_NS**2 that I could find. I do still have a few instances of rho_c that do not have an accompanying Geometrized unit. The output plot gives a negative pressure for the improved model, and a near zero value for p_degen.

--- NSMass.py
+++ NSMass.py
@@ -11,11 +11,21 @@ R_NS = 11400 # Approximate radius in m
 
 # Mass 
 r = np.linspace(0, R_NS, num = 1001) # in m
+
+xi = (r/R_NS) # given without units
     
 M_BH = (r*(c**2))/(2*G) # in Kg
-M_Tol = (M_NS)*(((5/2)*(r/(R_NS)*10**3)**3) - (3/2)*(r/(R_NS)*10**3)**5) # in Kg
+M_Tol = (M_NS)*(((5/2)*xi**3) - (3/2)*xi**5) # in Kg
 
 outFile = open("PhantomBlackHoleMassData.txt", "w")
 for i in range(0, 1001):
     outFile.write(str(M_BH[i]) + " " + str(M_Tol[i]) + " " + str(r[i]) + "\n")
 outFile.close() 
+
+plt.plot(r, M_BH)
+plt.plot(r, M_Tol)
+plt.legend(["M_BH", "M_Tol"])
+plt.xlabel("Radii (m)")
+plt.ylabel("Enclosed Mass (Kg)")
+plt.savefig("MassPlot.pdf")
+plt.close()
--- NSPressure.py
+++ NSPressure.py
@@ -13,7 +13,8 @@ M_NS = 1.4*1.9891E30 # Mass of neutron star in kg
 r = np.linspace(0, R_NS, 1001)
 xi = r/R_NS
 rho_c = (15.0*M_NS)/(8.0*math.pi*R_NS**3)
-C = ((8.0*math.pi/15.0)*R_NS**2*rho_c)*(G/c**2)
+rho = (R_NS**2)*(rho_c)*(G/c**2)
+C = ((8.0*math.pi/15.0)*rho)
 
 C_2 = math.atan(math.sqrt(C/(3.0*(1.0 - 2.0*C)))) + 0.5*math.log((1.0/6.0) + math.sqrt((1.0-2.0*C)/(3.0*C)))
 E_lamb = 1.0 - C*xi**2*(5.0 - 3.0*xi**2)
@@ -22,6 +23,7 @@ phi_tol = C_2 - 0.5*np.log(xi**2 - (5.0/6.0) + np.sqrt(E_lamb/(3.0*C)))
 p_tol = (1.0/(4.0*math.pi*R_NS**2))*((np.sqrt(3.0*C*E_lamb)*np.tan(phi_tol) - C*(5.0 - 3.0*xi**2)/2.0)/(4.0*math.pi*R_NS**2))
 
 p_c = 0.9*p_tol[0]
+
 print(p_c)
 
 #Improved Tolman Solution
@@ -33,12 +35,21 @@ a_1 = (-1.50266)
 a_2 = 0.0643875
 n = 0.903
 
-alpha = a_0 + a_1*((C**n)/(rho_c*R_NS**2)) + a_2*((C**n)/(rho_c*R_NS**2))**2
-C_2imp = math.atan(-(2.0*(10.0 - 3.0*alpha)*R_NS*math.sqrt(6.0*math.pi*rho_c*(15.0 - 16.0*math.pi*rho_c*R_NS**2)))/((48.0*math.pi*(10.0 - 3.0*alpha)*rho_c*R_NS) - 315.0)) + 0.5*math.log((1.0/6.0) + math.sqrt((5.0/(8.0*math.pi*rho_c*R_NS**2)) - (2.0/3.0)))
-phi_imp = C_2imp - 0.5*np.log(xi**2 - (5.0/6.0) + np.sqrt((5.0*E_lamb)/(8*math.pi*rho_c*R_NS**2)))
+alpha = a_0 + a_1*((C**n)/(rho)) + a_2*((C**n)/(rho))**2
+C_2imp = math.atan(-(2.0*(10.0 - 3.0*alpha)*math.sqrt(6.0*math.pi*rho*(15.0 - 16.0*math.pi*rho)))/((48.0*math.pi*(10.0 - 3.0*alpha)*rho) - 315.0)) + 0.5*math.log((1.0/6.0) + math.sqrt((5.0/(8.0*math.pi*rho)) - (2.0/3.0)))
+phi_imp = C_2imp - 0.5*np.log(xi**2 - (5.0/6.0) + np.sqrt((5.0*E_lamb)/(8*math.pi*rho)))
 
-p_imp = (np.sqrt(E_lamb_tol*rho_c/(10.0*math.pi))*(np.tan(phi_imp)/R_NS)) + ((1.0/15.0)*((3.0*xi**2) - 5.0)*rho_c) + ((6.0*(1.0-alpha)*rho_c)/((16.0*math.pi*(10.0 - (3.0*alpha))*rho_c*R_NS**2) - 105))
+p_imp = (np.sqrt(E_lamb*rho_c/(10.0*math.pi))*(np.tan(phi_imp)/R_NS)) + ((1.0/15.0)*((3.0*xi**2) - 5.0)*rho_c) + ((6.0*(1.0-alpha)*rho_c)/((16.0*math.pi*(10.0 - (3.0*alpha))*rho) - 105))
+
+#Neutron Degeneracy Pressure
+
+p_degen = (((((math.pi)**3)*(1.054E-34)**2)/(15*(1.675E-27)))*(((3*(M_NS))/(1.675E-27))/(((math.pi)**2)*(4/3)*(R_NS*(10**3))**3))**(5/3))*(G/c**4) # in Geometric Units
+p_deg = np.empty(1001)
+p_deg.fill(p_degen)
+
+#Plotting
 
 plt.plot(r/R_NS, p_tol/p_c)
 plt.plot(r/R_NS, p_imp/p_c)
+plt.plot(r/R_NS, p_deg/p_c)
 plt.show()
